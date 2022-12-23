35fbe0eeb11478ee70f3eb354c357d55a1ea24cc
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-24 01:57:49 +0400

    use body parser for response json

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import moviesRouter from "./routers/movies-router.js";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
 dotenv.config();
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.use("/api", moviesRouter);
 
