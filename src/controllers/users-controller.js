ca6bf55752c445e89bf82daf478a601bbba82480
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-24 02:26:29 +0400

    create user post request

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,6 +1,6 @@
 import pool from "../config/dataBase.js";
 
-const getAllUsers = async (req, res) => {
+export const getAllUsers = async (req, res) => {
   try {
     const allUsers = await pool.query("SELECT * FROM users");
     res.json(allUsers.rows);
@@ -9,4 +9,15 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-export default getAllUsers;
+export const postUser = async (req, res) => {
+  try {
+    const { user_id, user_email, user_password } = req.body;
+    const user = await pool.query(
+      `INSERT INTO users (user_id, user_email, user_password) VALUES($1, $2, $3) RETURNING *`,
+      [user_id, user_email, user_password]
+    );
+    res.json(user.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- src/routers/users-router.js
+++ src/routers/users-router.js
@@ -1,8 +1,9 @@
-import getAllUsers from "../controllers/users-controller.js";
+import { getAllUsers, postUser } from "../controllers/users-controller.js";
 import express from "express";
 
 const usersRouter = express.Router();
 
 usersRouter.get("/users", getAllUsers);
+usersRouter.post("/users", postUser);
 
 export default usersRouter;
